092dd21303d2f773985a15929424e6acbc2002f8
Kumail Raza <kumailraza260@gmail.com>
2023-12-14 11:05:56 +0500

    Flutter Provider Stateless

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+   HomeScreen({super.key});
+   int x =0;
   @override
   Widget build(BuildContext context) {
     // ignore: avoid_print
@@ -11,11 +11,22 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Flutter Provider"),
       ),
-      body: const Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Container(
+            child: Center(child: Text(x.toString(),style: TextStyle(fontSize: 50),)),
+          ),
 
         ],
       ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        x++;
+        print(x);
+
+      },
+      child: const Icon(Icons.add),),
     );
   }
 }
